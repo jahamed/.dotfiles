93bbd3fdadeef1d225d11fea089b03d589db830b
Javed <jahamed@avvo.com>
2017-06-30 13:48:48 -0700

    add hammerspoon and avvo env

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,638 @@
+-- init.lua
+
+-- Richard Zhao <richardz@andrew.cmu.edu>
+-- Modified by Javed Ahamed
+
+local log = hs.logger.new('init', 'debug')
+log.i('initializing')
+
+-- LUA HELPERS
+function Set(t)
+  local s = {}
+  for _,v in pairs(t) do s[v] = true end
+  return s
+end
+
+function contains(t, e)
+  return t[e]
+end
+
+-- END LUA HELPERS
+
+--------------------------------------------------------------------------------
+-- General ---------------------------------------------------------------------
+
+local hyper = {'cmd', 'alt', 'ctrl', 'shift'}
+
+local hotkey = require 'hs.hotkey'
+
+-- local alertStyle = {
+--    radius = 0,
+--    strokeWidth = 0,
+--    fillColor = { white = 0.1, alpha = 0.8 },
+-- }
+
+hs.alert.defaultStyle.radius = 2
+hs.alert.defaultStyle.textSize = 22
+hs.alert.defaultStyle.textFont = 'Menlo'
+hs.alert.defaultStyle.textColor = { white = 1, alpha = 0.95 }
+hs.alert.defaultStyle.strokeColor = { white = 1, alpha = 0 }
+hs.alert.defaultStyle.fillColor = { white = 0.2, alpha = 0.9 }
+
+-- Configuration reload
+hotkey.bind(hyper, 'r', function ()
+  hs.reload()
+end)
+hs.alert.show('Config loaded')
+
+--------------------------------------------------------------------------------
+-- Scripts ---------------------------------------------------------------------
+
+
+-- Open a new iTerm window
+hotkey.bind(hyper, 't', function ()
+  local iterm = [[ tell application "iTerm"
+  create window with default profile
+end tell
+]]
+  hs.osascript.applescript(iterm)
+end)
+
+-- Open a new Finder window
+hotkey.bind(hyper, 'n', function ()
+  local finder = [[ tell application "Finder"
+    make new Finder window
+end tell
+do shell script "open -a \"Finder\""
+]]
+  hs.osascript.applescript(finder)
+end)
+
+
+--------------------------------------------------------------------------------
+-- Focus -----------------------------------------------------------------------
+
+hotkey.bind(hyper, 'h', function ()
+  hs.window.focusWindowWest()
+end)
+
+hotkey.bind(hyper, 'j', function ()
+  hs.window.focusWindowSouth()
+end)
+
+hotkey.bind(hyper, 'k', function ()
+  hs.window.focusWindowNorth()
+end)
+
+hotkey.bind(hyper, 'l', function ()
+  hs.window.focusWindowEast()
+end)
+
+hotkey.bind(hyper, 'e', function ()
+  hs.application.launchOrFocus('/usr/local/Cellar/emacs/25.1/Emacs.app')
+end)
+
+hotkey.bind(hyper, 'i', function ()
+  hs.application.launchOrFocus('/Applications/iTerm.app')
+end)
+
+
+--------------------------------------------------------------------------------
+-- Window management -----------------------------------------------------------
+
+local screen = require 'hs.screen'
+local window = require 'hs.window'
+local geom   = require 'hs.geometry'
+
+local gap = 15
+local margin = {
+  left   = 20,
+  top    = 20,
+  right  = 20,
+  bottom = 20
+}
+
+local delta = 50                -- For resizing and moving
+
+local resizer = {}
+local mover   = {}
+
+function box ()
+  return screen.mainScreen():currentMode()
+end
+
+function fWin ()
+  return window.focusedWindow()
+end
+
+function resizer.full ()
+  local win = fWin ()
+  local b = box ()
+  local w = b.w - margin.left - margin.right
+  local h = b.h - margin.top - margin.bottom
+
+  win:move(geom(margin.left, margin.top, w, h), nil, nil, 0)
+end
+
+function resizer.center ()
+  local win = fWin ()
+  local b = box ()
+  local l = b.w / 6
+  local t = b.h / 6
+  local w = b.w * 2 / 3
+  local h = b.h * 2 / 3
+
+  win:move(geom(l, t, w, h), nil, nil, 0)
+end
+
+function resizer.reading ()
+  local win = fWin ()
+  local b = box ()
+  local l = b.w / 6
+  local t = margin.top
+  local w = b.w * 2 / 3
+  local h = b.h - margin.top - margin.bottom
+
+  win:move(geom(l, t, w, h), nil, nil, 0)
+end
+
+function resizer.left ()
+  local win = fWin ()
+  local b = box ()
+  local w = b.w / 2 - margin.left - gap / 2
+  local h = b.h - margin.top - margin.bottom
+
+  win:move(geom(margin.left, margin.top, w, h), nil, nil, 0)
+end
+
+function resizer.right ()
+  local win = fWin ()
+  local b = box ()
+  local l = b.w / 2 + gap / 2
+  local t = margin.top
+  local w = b.w / 2 - margin.left - gap / 2
+  local h = b.h - margin.top - margin.bottom
+
+  win:move(geom(l, t, w, h), nil, nil, 0)
+end
+
+function resizer.northEast ()
+  local win = fWin ()
+  local b = box ()
+  local t = margin.top
+  local w = b.w / 2 - margin.left - gap / 2
+  local h = b.h / 2 - margin.top - gap / 2
+
+  win:move(geom(margin.left, t, w, h), nil, nil, 0)
+end
+
+function resizer.southEast ()
+  local win = fWin ()
+  local b = box ()
+  local t = b.h / 2 + gap / 2
+  local w = b.w / 2 - margin.left - gap / 2
+  local h = b.h / 2 - margin.bottom - gap / 2
+
+  win:move(geom(margin.left, t, w, h), nil, nil, 0)
+end
+
+function resizer.northWest ()
+  local win = fWin ()
+  local b = box ()
+  local l = b.w / 2 + gap / 2
+  local t = margin.top
+  local w = b.w / 2 - margin.left - gap / 2
+  local h = b.h / 2 - margin.top - gap / 2
+
+  win:move(geom(l, t, w, h), nil, nil, 0)
+end
+
+function resizer.southWest ()
+  local win = fWin ()
+  local b = box ()
+  local l = b.w / 2 + gap / 2
+  local t = b.h / 2 + gap / 2
+  local w = b.w / 2 - margin.left - gap / 2
+  local h = b.h / 2 - margin.bottom - gap / 2
+
+  win:move(geom(l, t, w, h), nil, nil, 0)
+end
+
+function resizer.shrinkLeft ()
+  local win = fWin ()
+  local s = win:size()
+  local w = s.w - delta
+
+  win:setSize(geom({w = w, h = s.h}))
+end
+
+function resizer.shrinkUp ()
+  local win = fWin ()
+  local s = win:size()
+  local h = s.h - delta
+
+  win:setSize(geom({w = s.w, h = h}))
+end
+
+function resizer.expandRight ()
+  local win = fWin ()
+  local s = win:size()
+  local w = s.w + delta
+
+  win:setSize(geom({w = w, h = s.h}))
+end
+
+function resizer.expandDown ()
+  local win = fWin ()
+  local s = win:size()
+  local h = s.h + delta
+
+  win:setSize(geom({w = s.w, h = h}))
+end
+
+function mover.left ()
+  local win = fWin ()
+
+  win:move(geom(-delta, 0), nil, true)
+end
+
+function mover.right ()
+  local win = fWin ()
+
+  win:move(geom(delta, 0), nil, true)
+end
+
+function mover.up ()
+  local win = fWin ()
+
+  win:move(geom(0, -delta), nil, true)
+end
+
+function mover.down ()
+  local win = fWin ()
+
+  win:move(geom(0, delta), nil, true)
+end
+
+
+hotkey.bind(hyper, 'f',     resizer.full)
+hotkey.bind(hyper, 'c',     resizer.center)
+hotkey.bind(hyper, 'left',  resizer.left)
+hotkey.bind(hyper, 'right', resizer.right)
+hotkey.bind(hyper, '-', resizer.northEast)
+hotkey.bind(hyper, '=', resizer.northWest)
+hotkey.bind(hyper, '[', resizer.southEast)
+hotkey.bind(hyper, ']', resizer.southWest)
+
+hotkey.bind({ 'alt' }, 'left',  resizer.shrinkLeft, nil, resizer.shrinkLeft)
+hotkey.bind({ 'alt' }, 'right', resizer.expandRight, nil, resizer.expandRight)
+hotkey.bind({ 'alt' }, 'up',    resizer.shrinkUp, nil, resizer.shrinkUp)
+hotkey.bind({ 'alt' }, 'down',  resizer.expandDown, nil, resizer.expandDown)
+
+hotkey.bind({ 'alt', 'shift' }, 'h',  mover.left,  nil, mover.left)
+hotkey.bind({ 'alt', 'shift' }, 'j',  mover.down,  nil, mover.down)
+hotkey.bind({ 'alt', 'shift' }, 'k',  mover.up,    nil, mover.up)
+hotkey.bind({ 'alt', 'shift' }, 'l',  mover.right, nil, mover.right)
+
+
+--------------------------------------------------------------------------------
+-- Drawing ---------------------------------------------------------------------
+
+local drawing = require 'hs.drawing'
+local timer   = require 'hs.timer'
+
+local hints = {
+  -- font = 'Lato Light',
+  font = 'Lato Light',
+  size = 24,
+  width = 200,
+  duration = 2,
+  fadeIn = 0.3,
+  fadeOut = 0.3,
+}
+
+local colors = {
+  orange = { red = 214 / 255, green = 93 / 255, blue = 14 / 255, alpha = 0.9 },
+  green  = { red = 152 / 255, green = 151 / 255, blue = 26 / 255, alpha = 0.9 },
+  blue = { red = 69 / 255, green = 133 / 255, blue = 136 / 255, alpha = 0.9 },
+  grey = { red = 0, green = 0, blue = 0, alpha = 0.4 }
+}
+
+-- Create a circle centered at (x, y) with radius r.
+function hints.circle (x, y, r, color)
+  local c = drawing.circle(geom(x - r, y - r, 2 * r, 2 * r))
+  c:setStroke(false)
+  c:setFillColor(color)
+  return c
+end
+
+function hints.background ()
+  local b = box ()
+  local rect = drawing.rectangle(geom(0, 0, b.w, b.h))
+                 :setStroke(false)
+                 :setFillColor(colors.grey)
+  return {
+    show = function ()
+      rect:show(hints.fadeIn)
+    end,
+    hide = function ()
+      rect:hide(hints.fadeOut)
+    end
+  }
+end
+
+function hints.hint (x, y, key, msg, color)
+  local radius = hints.size
+  local circle = hints.circle(x + radius, y + radius, radius, color)
+  local keyBox = hs.drawing.getTextDrawingSize(key, {
+    font = hints.font, size = hints.size, alignment = 'center',
+  })
+  keyBox.w = keyBox.w + 3 -- known fudge factor (see getTextDrawingSize docs)
+
+  local left = x + radius - keyBox.w / 2
+  local top  = y + radius - keyBox.h / 2 - 5 -- lower case keys
+
+  local keyText = hs.drawing.text(geom(left, top, keyBox.w, keyBox.h), key)
+                   :setTextFont(hints.font)
+                   :setTextSize(hints.size)
+
+  local msgBox = hs.drawing.getTextDrawingSize(msg, {
+    font = hints.font, size = hints.size, alignment = 'left',
+  })
+  msgBox.w = msgBox.w + hints.size
+  if msgBox.w > hints.width then
+    log.i('width ' .. msgBox.w .. ' of hint ' .. msg .. ' too large')
+  end
+
+  local msgText = hs.drawing.text(geom(left + hints.size * 2, top, hints.width, msgBox.h), msg)
+                    :setTextFont(hints.font)
+                    :setTextSize(hints.size)
+
+  return {
+    show = function ()
+      circle:show(hints.fadeIn)
+      keyText:show(hints.fadeIn)
+      msgText:show(hints.fadeIn)
+    end,
+    hide = function ()
+      circle:hide(hints.fadeOut)
+      keyText:hide(hints.fadeOut)
+      msgText:hide(hints.fadeOut)
+    end
+  }
+end
+
+
+--------------------------------------------------------------------------------
+-- Hydra -----------------------------------------------------------------------
+
+local hydraExitKey = 'space'
+
+function hydraBatteryStatusTrigger ()
+  local minutes = hs.battery.timeRemaining ()
+  local hours = minutes // 60
+  local msg
+
+  if minutes == -2 then
+    msg = 'Power source: power adapter'
+  else
+    minutes = minutes % 60
+    msg = string.format('%d:%02d remaining', hours, minutes)
+  end
+  hs.alert.show(msg)
+end
+
+function hydraBrightnessIncrease ()
+  local current = hs.brightness.get()
+  local target  = math.min(current + 10, 100)
+  hs.brightness.set(target)
+end
+
+function hydraBrightnessDecrease ()
+  local current = hs.brightness.get()
+  local target  = math.max(current - 10, 0)
+  hs.brightness.set(target)
+end
+
+local hydraDefinitions = {
+  {
+    mods  = hyper,
+    key   = 'space',
+    hint  = 'head',
+    master = true,
+    actions = {
+      { mod = '', key = 'A', hint = 'Asana', target = function() webApp('https://app.asana.com') end, exit = true },
+      { mod = '', key = 'M', hint = 'Messenger', target = function() webApp('https://messenger.com') end, exit = true },
+      { mod = '', key = 'S', hint = 'Slack', target = function() hs.application.launchOrFocus('Slack')  end, exit = true },
+      { mod = '', key = 'T', hint = 'Terminal', target = function() newTerm() end, exit = true },
+      },
+    hydras = {
+      -- window
+      {
+        mods  = '',
+        key   = 'w',
+        hint  = 'window',
+        actions = {
+          { mod = '', key = 'c', hint = 'center', target = function() resizer.center() end, exit = false },
+          { mod = '', key = 'f', hint = 'full', target = function() resizer.full() end, exit = false },
+          { mod = '', key = 'r', hint = 'reading', target = function() resizer.reading() end, exit = false },
+        },
+        hydras = {}
+      },
+
+      -- launcher
+      {
+        mods  = '',
+        key   = 'l',
+        hint  = 'launch',
+        actions = {
+          { mod = '', key = 's', hint = 'Safari', target = function() hs.application.launchOrFocus('Safari') end, exit = true },
+          { mod = '', key = 't', hint = 'iTerm', target = function() hs.application.launchOrFocus('iTerm') end, exit = true },
+          { mod = '', key = 'p', hint = 'Spotify', target = function() hs.application.launchOrFocus('Spotify') end, exit = true }
+        },
+        hydras = {}
+      },
+
+      -- info
+      {
+        mods  = '',
+        key   = 'i',
+        hint  = 'info',
+        actions = {
+          { mod = '', key = 'b', hint = 'Battery', target = function() hydraBatteryStatusTrigger() end }
+        },
+        hydras = {}
+      },
+
+      -- system
+      {
+        mods  = '',
+        key   = 's',
+        hint  = 'system',
+        actions = {
+          { mod = '', key = 's', hint = 'Screen saver', target = function() hs.caffeinate.startScreensaver() end, exit = true }
+        },
+        hydras = {
+          {
+            mods  = '',
+            key   = 'b',
+            hint  = 'brightness',
+            actions = {
+              { mod = '', key = 'j', hint = '-', target = hydraBrightnessDecrease },
+              { mod = '', key = 'k', hint = '+', target = hydraBrightnessIncrease },
+            },
+            hydras = {}
+          }
+        }
+      }
+    }
+  }
+}
+
+function tableLen(T)
+  local count = 0
+  for k, v in pairs(T) do
+    count = count + 1
+  end
+  return count
+end
+
+function hydraGenerateHints (hydra)
+  local b = box ()
+  local hintTable = {}
+
+  local numHints = tableLen(hydra.hydras) + tableLen(hydra.actions)
+  local height = hints.size * 2 + hints.size / 2
+  local totalHeight = numHints * height
+
+  local left = b.w / 2 - hints.width / 2
+  local top  = b.h / 2 - totalHeight / 2
+
+  local y = top
+
+  -- generate child hydras first
+  for _, child in pairs(hydra.hydras) do
+    local hint = hints.hint(left, y, child.key, child.hint, colors.blue)
+    table.insert(hintTable, hint)
+    y = y + height
+  end
+
+  -- and then actions
+  for _, child in pairs(hydra.actions) do
+    local hint
+    if child.exit then
+      hint = hints.hint(left, y, child.key, child.hint, colors.orange)
+    else
+      hint = hints.hint(left, y, child.key, child.hint, colors.green)
+    end
+    table.insert(hintTable, hint)
+    y = y + height
+  end
+
+  return {
+    show = function ()
+      for _, hint in pairs(hintTable) do
+        hint.show()
+      end
+    end,
+    hide = function ()
+      for _, hint in pairs(hintTable) do
+        hint.hide()
+      end
+    end
+  }
+end
+
+function hydraExitAllAndHideHints ()
+  recurse = function (hydras)
+    for _, hydra in pairs(hydras) do
+      if hydra.master ~= nil and hydra.master == true then
+        hydra.background.hide()
+      end
+      hydra.modal:exit()
+      hydra.hints.hide()
+      if next(hydra.hydras) ~= nil then
+        recurse(hydra.hydras)
+      end
+    end
+  end
+  recurse(hydraDefinitions)
+end
+
+function hydraInit (parentHydra, hydras)
+  if next(hydras) == nil then return end
+
+  -- add modals to each body and bind all actions
+  for _, hydra in pairs(hydras) do
+    -- generate a new modal and put it under its parent
+    hydra.modal = hotkey.modal.new()
+    hydra.modal:bind('', hydraExitKey, function() hydraExitAllAndHideHints () end)
+
+    -- global bind of master
+    if hydra.master ~= nil and hydra.master == true then
+      hydra.background = hints.background ()
+      hotkey.bind(hydra.mods, hydra.key, function () 
+        hydra.background.show()
+        hydra.modal:enter() 
+      end)
+    else
+      parentHydra.modal:bind(hydra.mods, hydra.key, function() 
+        parentHydra.modal:exit()
+        hydra.modal:enter() 
+      end)
+    end
+
+    -- add the hint drawing objects
+    hydra.hints = hydraGenerateHints(hydra)
+
+    -- for debugging
+    function hydra.modal:entered()
+      hydra.hints.show()
+      log.i("hydra [" .. hydra.hint .. "] entered")
+    end
+    function hydra.modal:exited() 
+      hydra.hints.hide()
+      log.i("hydra [" .. hydra.hint .. "] exited") 
+    end
+
+    -- add actions
+    for _, action in ipairs(hydra.actions) do
+      hydra.modal:bind(action.mod, action.key, function ()
+        action.target ()
+        if action.exit then hydraExitAllAndHideHints () end
+      end)
+      log.i("hydra [" .. hydra.hint .. "] binding action " .. action.hint)
+    end
+
+    -- recurse on children
+    hydraInit (hydra, hydra.hydras)
+  end
+end
+
+-- Create all hydras under the head
+hydraInit (nil, hydraDefinitions)
+
+-- Wifi Events, turn volume off when on an unregonized WiFi
+wifiWatcher = nil
+-- homeSSID = "AvvoHQ"
+ssidSet = Set{'AvvoHQ', 'Tacocat'}
+lastSSID = hs.wifi.currentNetwork()
+
+function ssidChangedCallback()
+  newSSID = hs.wifi.currentNetwork()
+
+  if contains(ssidSet, newSSID) and not contains(ssidSet, lastSSID) then
+    -- We just joined our home WiFi network
+    hs.audiodevice.defaultOutputDevice():setVolume(25)
+    hs.alert.show("Unrecognized SSID - Setting Volume to 25")
+  elseif not contains(ssidSet, newSSID) and contains(ssidSet, lastSSID) then
+    -- We just departed our home WiFi network
+    hs.audiodevice.defaultOutputDevice():setVolume(0)
+    hs.alert.show("Unrecognized SSID - Setting Volume to 0")
+  end
+
+  lastSSID = newSSID
+end
+
+wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
+wifiWatcher :start()
---
+++ .hammerspoon/init_backup2.lua
@@ -0,0 +1,282 @@
+local hyper = {'cmd', 'alt', 'ctrl', 'shift'}
+local cmdAltCtl = {"cmd", "alt", "ctrl"}
+
+-- LUA HELPERS
+function Set(t)
+  local s = {}
+  for _,v in pairs(t) do s[v] = true end
+  return s
+end
+
+function contains(t, e)
+  return t[e]
+end
+
+-- END LUA HELPERS
+
+-- Configuration reload
+hs.hotkey.bind(hyper, 'r', function ()
+  -- appWatcher:stop()
+  hs.reload()
+end)
+hs.alert.show('Config loaded')
+
+-- Automatic Conflig Reload
+-- function reloadConfig(files)
+--     doReload = false
+--     for _,file in pairs(files) do
+--         if file:sub(-4) == ".lua" then
+--             doReload = true
+--         end
+--     end
+--     if doReload then
+--         hs.reload()
+--     end
+-- end
+-- myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+-- hs.alert.show("Config loaded")
+
+-- Hello World (PopUp)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.alert.show("Hello World!")
+end)
+
+-- Fancy Hello World (OSX Sidebar Notification)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+end)
+
+-- Complex window movement (NetHack Style Keys)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Y", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  f.y = f.y - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.y = f.y - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x + 10
+  f.y = f.y - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "B", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  f.y = f.y + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.y = f.y + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x + 10
+  f.y = f.y + 10
+  win:setFrame(f)
+end)
+
+-- Move window to left
+hs.hotkey.bind(cmdAltCtl, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Move window to right
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Move window to top
+hs.hotkey.bind(cmdAltCtl, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(cmdAltCtl, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- How to make a preset layout
+local laptopScreen = "Color LCD"
+local windowLayout = {
+  -- {"Google Chrome", nil, laptopScreen, hs.layout.left50, nil, nil}
+}
+hs.layout.apply(windowLayout)
+
+-- Check current spotify song
+hs.hotkey.bind(hyper, "S", function()
+  hs.spotify.displayCurrentTrack()
+end)
+
+-- Caffeine Menubar 
+caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+    if state then
+        caffeine:setTitle("AWAKE")
+    else
+        caffeine:setTitle("SLEEPY")
+    end
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+
+-- React to application events
+function applicationWatcher(appName, eventType, appObject)
+    if (eventType == hs.application.watcher.activated) then
+        if (appName == "Finder") then
+            -- Bring all Finder windows forward when one gets activated
+            appObject:selectMenuItem({"Window", "Bring All to Front"})
+        end
+    end
+end
+
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
+
+-- Wifi Events, turn volume off when on an unregonized WiFi
+wifiWatcher = nil
+-- homeSSID = "AvvoHQ"
+ssidSet = Set{'AvvoHQ', 'Tacocat'}
+lastSSID = hs.wifi.currentNetwork()
+
+function ssidChangedCallback()
+  newSSID = hs.wifi.currentNetwork()
+
+  if contains(ssidSet, newSSID) and not contains(ssidSet, lastSSID) then
+    -- We just joined our home WiFi network
+    hs.audiodevice.defaultOutputDevice():setVolume(25)
+    hs.alert.show("Setting Volume to 25")
+  elseif not contains(ssidSet, newSSID) and contains(ssidSet, lastSSID) then
+    -- We just departed our home WiFi network
+    hs.audiodevice.defaultOutputDevice():setVolume(0)
+    hs.alert.show("Setting Volume to 0")
+  end
+
+  lastSSID = newSSID
+end
+
+wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
+wifiWatcher :start()
+
+-- Defeat Paste Blocking
+hs.hotkey.bind({"cmd", "alt"}, "V", function()
+  hs.eventtap.keyStrokes(hs.pasteboard.getContents()) 
+end)
+
+mouseCircle = nil
+mouseCircleTimer = nil
+
+-- Draw the cursor 
+function mouseHighlight()
+    -- Delete an existing highlight if it exists
+    if mouseCircle then
+        mouseCircle:delete()
+        if mouseCircleTimer then
+            mouseCircleTimer:stop()
+        end
+    end
+    -- Get the current co-ordinates of the mouse pointer
+    mousepoint = hs.mouse.getAbsolutePosition()
+    -- Prepare a big red circle around the mouse pointer
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+    mouseCircle:setFill(false)
+    mouseCircle:setStrokeWidth(5)
+    mouseCircle:show()
+
+    -- Set a timer to delete the circle after 3 seconds
+    mouseCircleTimer = hs.timer.doAfter(3, function() mouseCircle:delete() end)
+end
+
+hs.hotkey.bind(hyper, "D", mouseHighlight)
+
+
+
+
+
+
+
+
+
+
